ffa70ef34083a61d5019fdf69ddd846782b38113
Immortal230719 <Immortal230719@gmail.com>
2020-06-01 13:40:33 +0300

    Added icon in Title

--- src/components/Title/styles/index.ts
+++ src/components/Title/styles/index.ts
@@ -1,17 +1,25 @@
 import styled, { css } from 'styled-components';
 import { device } from 'styles';
 
-const Title = styled.h1`
+export const StyledTitle = styled.h1`
   ${({ theme }) => css`
+    position: relative;
     color: ${theme.colors.white};
     text-align: center;
-    margin: 50px 0;
     font-size: 40px;
+    width: fit-content;
+
+    img {
+      position: absolute;
+      top: -30px;
+      right: 0;
+      transform: translate(120%, 0);
+      width: 80px;
+      height: 80px;
+    }
 
     @media ${device.mobileL} {
       font-size: 26px;
     }
   `}
 `;
-
-export default Title;
